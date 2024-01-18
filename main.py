8ee5522a3ba74e56fabebf2fd4e2263af7585b2a
Darija Rumjanceva <darija.rumjanceva@edu.rtu.lv>
2024-01-18 18:37:54 +0200

    Implemented web parsing with Selenium Webdriver

--- main.py
+++ main.py
@@ -6,22 +6,33 @@ import pandas as pd
 
 from openpyxl import Workbook, load_workbook
 
+#Excel
 wb = load_workbook('WIEBETECH PRICES.xlsx')
 ws = wb.active
 max_row = ws.max_row
 total = 0
 
+product_codes = []
+
+for row in range(2, max_row + 1):
+    product_code = ws[f'A{row}'].value
+    if product_code is not None:
+        product_codes.append(product_code)
+
+#Web
 service = Service()
 option = webdriver.ChromeOptions()
 driver = webdriver.Chrome(service=service, options=option)
 
-url = "https://emn178.github.io/online-tools/sha256.html"
-driver.get(url)
-time.sleep(2)
+base_url = 'https://wiebetech.com'
+code = "7100-3000-11"
 
-find = driver.find_element(By.LINK_TEXT, "SHA224")
-find.click()
-delay = 2
-WebDriverWait(driver, delay).until(EC.presence_of_element_located((By.ID, "input")))
-print("objekts ir uz ekrana")
-input()
+def get_product_link(product_code):
+    search_url = f"{base_url}?s={product_code}"
+    page = driver.get(search_url)
+    find = driver.find_element(By.XPATH, "//*[@id='content']/div/div/div/div[1]/div[1]/a")
+    find.click()
+    product_url = driver.current_url
+    return product_url
+
+print (get_product_link(code))
